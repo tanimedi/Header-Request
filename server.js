17c9093221d0f023d3c7135067f47790dbbe8c4d
Tanisha <tjmedi87@gmail.com>
2021-07-11 15:29:38 -0400

    add headers

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var express = require('express');
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+// so that your API is remotely testable by FCC
 var cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
@@ -20,11 +20,19 @@ app.get("/", function (req, res) {
 });
 
 
-// your first API endpoint... 
+// your first API endpoint...
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+//Header Request
+app.get("/api/whoami", function (req, res) {
+  res.json({
+    ipaddress: req.connection.remoteAddress,
+    language: req.headers["accept-language"],
+    software: req.headers["user-agent"]
+  });
+});
 
 
 // listen for requests :)
